8d5b77ae53f470789fce2c7d5ae6cbd636c87c6d
freeman <the.freeman.lab@gmail.com>
2015-01-16 14:00:30 -0500

    Var name bug fix

--- line/index.js
+++ line/index.js
@@ -16,10 +16,10 @@ var margin = {
 
 
 var nestedExtent = function(data, map) {
-    var max = d3.max(arrays, function(arr) {
+    var max = d3.max(data, function(arr) {
         return d3.max(_.map(arr, map));
     });
-    var min = d3.min(arrays, function(arr) {
+    var min = d3.min(data, function(arr) {
         return d3.min(_.map(arr, map));
     });
 
