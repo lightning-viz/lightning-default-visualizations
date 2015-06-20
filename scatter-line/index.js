d507358820911fc42efa26dbae699db06a7af4d8
freeman <the.freeman.lab@gmail.com>
2015-06-20 16:56:02 -0400

    Improve relative sizing

--- scatter-line/index.js
+++ scatter-line/index.js
@@ -16,7 +16,7 @@ var ScatterLine = function(selector, data, images, options) {
     this.$el = $el;
 
     var Scatter = require('../viz/scatter');
-    var scatter = new Scatter(selector + ' #scatter-line-' + this.id + ' .scatter', data, null, {width: $(selector).width(), height: Math.min(500, $(selector).width * 0.6)});
+    var scatter = new Scatter(selector + ' #scatter-line-' + this.id + ' .scatter', data, null, {width: $(selector).width(), height: 400});
     var Line = require('../viz/line');
     var line;
 
@@ -25,7 +25,7 @@ var ScatterLine = function(selector, data, images, options) {
         if(!err) {
             var series = data
             var newdata = {'series': _.times(series.length, _.constant(0))};
-            line = new Line(selector + ' #scatter-line-' + self.id + ' .line', newdata, null, {width: $(selector).width(), height: 300, zoomAxes: ['x']});
+            line = new Line(selector + ' #scatter-line-' + self.id + ' .line', newdata, null, {width: $(selector).width(), height: 200, zoomAxes: ['x']});
         } else {
             line = new Line(selector + ' #scatter-line-' + self.id + ' .line', {series: [0]}, null, {width: $(selector).width(), height: 300, zoomAxes: ['x']});
         }
